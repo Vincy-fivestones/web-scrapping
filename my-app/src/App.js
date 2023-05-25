0044feaf5080b0bfa9350fd53c0bec4378de8098
Vincy-fivestones <vincy.c@fivestones.net>
2023-05-25 16:00:26 +0800

    use react-query to handle error
    Add update the UI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,85 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import Alert, { AlertColor } from '@mui/material/Alert';
 import Button from '@mui/material/Button';
-import axios from "axios";
+import axios from 'axios';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [urls, setUrls] = useState("");
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Form></Form>
+    </QueryClientProvider>
+  );
+}
 
-  console.log("");
+function Form() {
+  const queryClient = useQueryClient();
+  const [urls, setUrls] = useState('');
+  const [msg, setMsg] = useState({ type: 'info', message: '' });
+
+  console.log('');
+  const transfer = async () => {
+    const response = await axios.post(
+      '/scrape',
+      { urls: urls },
+      { responseType: 'arraybuffer' },
+    );
+    return response;
+  };
+
+  const mutation = useMutation(transfer, {
+    onError: (error, variables, context) => {
+      // An error happened!
+      console.log(new TextDecoder('utf-8').decode(error.response.data));
+      setMsg({
+        type: 'error',
+        message: new TextDecoder('utf-8').decode(error.response.data),
+      });
+    },
+    onSuccess: (data, variables, context) => {
+      const url = window.URL.createObjectURL(new Blob([data.data]), {
+        type: 'application/zip',
+      });
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'scrapped_files.zip');
+      document.body.appendChild(link);
+      link.click();
+      setMsg({ type: 'success', message: 'Scrapped successfully' });
+    },
+  });
+
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    axios
-      .post("/scrape", { urls: urls }, { responseType: 'arraybuffer' })
-      .then((response) => {
-        // Create a link to the file and click it to trigger the download
-        const url = window.URL.createObjectURL(new Blob([response.data]), { type: 'application/zip' });
-        const link = document.createElement("a");
-        link.href = url;
-        link.setAttribute("download", "scrapped_files.zip");
-        document.body.appendChild(link);
-        link.click();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    if (urls.length > 0) {
+      mutation.mutateAsync();
+    } else {
+      setMsg({ type: 'warning', message: 'Please enter at least 1 url' });
+    }
   };
 
   return (
-    <Container maxWidth="md" style={{ marginTop: "3rem" }}>
+    <Container maxWidth="md" style={{ marginTop: '3rem' }}>
+      {msg.message && (
+        <Alert
+          severity={msg.type}
+          onClose={() => {
+            setMsg({ type: 'info', message: '' });
+          }}
+        >
+          {msg.message}
+        </Alert>
+      )}
       <form onSubmit={handleSubmit}>
         <Typography variant="h5" gutterBottom>
           Web Scraping Tool
@@ -48,7 +98,6 @@ function App() {
         </Button>
       </form>
     </Container>
-
   );
 }
 
